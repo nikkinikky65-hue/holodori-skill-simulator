7ed4f5596982744c1aa17f4c2e1bb2cdce02e3f0
nikkinikky65-hue <nikkinikky65@gmail.com>
2026-09-20 09:55:10 +0900

    Update members.js

--- members.js
+++ members.js
@@ -1,27 +1,301 @@
 const HOLO_MEMBERS = [
+  // ===== 0期生 =====
+  {
+    id: 'tokino_sora',
+    name: 'ときのそら',
+    affiliations: ['0期生']
+  },
+  {
+    id: 'robocosan',
+    name: 'ロボ子さん',
+    affiliations: ['0期生']
+  },
+  {
+    id: 'azki',
+    name: 'AZKi',
+    affiliations: ['0期生']
+  },
   {
     id: 'sakura_miko',
     name: 'さくらみこ',
-    generation: ''
+    affiliations: ['0期生']
   },
+  {
+    id: 'hoshimachi_suisei',
+    name: '星街すいせい',
+    affiliations: ['0期生']
+  },
+
+  // ===== 1期生 =====
+  {
+    id: 'aki_rosenthal',
+    name: 'アキ・ローゼンタール',
+    affiliations: ['1期生']
+  },
+  {
+    id: 'akai_haato',
+    name: '赤井はあと',
+    affiliations: ['1期生']
+  },
+  {
+    id: 'shirakami_fubuki',
+    name: '白上フブキ',
+    affiliations: ['1期生', 'ゲーマーズ']
+  },
+  {
+    id: 'natsuiro_matsuri',
+    name: '夏色まつり',
+    affiliations: ['1期生']
+  },
+
+  // ===== 2期生 =====
+  {
+    id: 'nakiri_ayame',
+    name: '百鬼あやめ',
+    affiliations: ['2期生']
+  },
+  {
+    id: 'yuzuki_choco',
+    name: '癒月ちょこ',
+    affiliations: ['2期生']
+  },
+  {
+    id: 'oozora_subaru',
+    name: '大空スバル',
+    affiliations: ['2期生']
+  },
+
+  // ===== ゲーマーズ =====
+  {
+    id: 'ookami_mio',
+    name: '大神ミオ',
+    affiliations: ['ゲーマーズ']
+  },
+  {
+    id: 'nekomata_okayu',
+    name: '猫又おかゆ',
+    affiliations: ['ゲーマーズ']
+  },
+  {
+    id: 'inugami_korone',
+    name: '戌神ころね',
+    affiliations: ['ゲーマーズ']
+  },
+
+  // ===== 3期生 =====
+  {
+    id: 'usada_pekora',
+    name: '兎田ぺこら',
+    affiliations: ['3期生']
+  },
+  {
+    id: 'shiranui_flare',
+    name: '不知火フレア',
+    affiliations: ['3期生']
+  },
+  {
+    id: 'shirogane_noel',
+    name: '白銀ノエル',
+    affiliations: ['3期生']
+  },
+  {
+    id: 'houshou_marine',
+    name: '宝鐘マリン',
+    affiliations: ['3期生']
+  },
+
+  // ===== 4期生 =====
   {
     id: 'tsunomaki_watame',
     name: '角巻わため',
-    generation: ''
+    affiliations: ['4期生']
   },
   {
-    id: 'fuwawa_abyssgard',
-    name: 'フワワ・アビスガード',
-    generation: ''
+    id: 'tokoyami_towa',
+    name: '常闇トワ',
+    affiliations: ['4期生']
+  },
+  {
+    id: 'himemori_luna',
+    name: '姫森ルーナ',
+    affiliations: ['4期生']
+  },
+
+  // ===== 5期生 =====
+  {
+    id: 'yukihana_lamy',
+    name: '雪花ラミィ',
+    affiliations: ['5期生']
+  },
+  {
+    id: 'momosuzu_nene',
+    name: '桃鈴ねね',
+    affiliations: ['5期生']
+  },
+  {
+    id: 'shishiro_botan',
+    name: '獅白ぼたん',
+    affiliations: ['5期生']
+  },
+  {
+    id: 'omaru_polka',
+    name: '尾丸ポルカ',
+    affiliations: ['5期生']
+  },
+
+  // ===== holoX =====
+  {
+    id: 'laplus_darknesss',
+    name: 'ラプラス・ダークネス',
+    affiliations: ['holoX']
+  },
+  {
+    id: 'takane_lui',
+    name: '鷹嶺ルイ',
+    affiliations: ['holoX']
+  },
+  {
+    id: 'hakui_koyori',
+    name: '博衣こより',
+    affiliations: ['holoX']
+  },
+  {
+    id: 'kazama_iroha',
+    name: '風真いろは',
+    affiliations: ['holoX']
+  },
+
+  // ===== ID1期生 =====
+  {
+    id: 'ayunda_risu',
+    name: 'アユンダ・リス',
+    affiliations: ['ID1期生']
+  },
+  {
+    id: 'moona_hoshinova',
+    name: 'ムーナ・ホシノヴァ',
+    affiliations: ['ID1期生']
+  },
+  {
+    id: 'airani_iofifteen',
+    name: 'アイラニ・イオフィフティーン',
+    affiliations: ['ID1期生']
+  },
+
+  // ===== ID2期生 =====
+  {
+    id: 'kureiji_ollie',
+    name: 'クレイジー・オリー',
+    affiliations: ['ID2期生']
+  },
+  {
+    id: 'anya_melfissa',
+    name: 'アーニャ・メルフィッサ',
+    affiliations: ['ID2期生']
+  },
+  {
+    id: 'pavolia_reine',
+    name: 'パヴォリア・レイネ',
+    affiliations: ['ID2期生']
+  },
+
+  // ===== ID3期生 =====
+  {
+    id: 'vestia_zeta',
+    name: 'ベスティア・ゼータ',
+    affiliations: ['ID3期生']
+  },
+  {
+    id: 'kaela_kovalskia',
+    name: 'カエラ・コヴァルスキア',
+    affiliations: ['ID3期生']
   },
   {
     id: 'kobo_kanaeru',
     name: 'こぼ・かなえる',
-    generation: ''
+    affiliations: ['ID3期生']
   },
+
+  // ===== Myth =====
+  {
+    id: 'mori_calliope',
+    name: '森カリオペ',
+    affiliations: ['Myth']
+  },
+  {
+    id: 'takanashi_kiara',
+    name: '小鳥遊キアラ',
+    affiliations: ['Myth']
+  },
+  {
+    id: 'ninomae_inanis',
+    name: '一伊那尓栖',
+    affiliations: ['Myth']
+  },
+
+  // ===== Promise =====
+  {
+    id: 'irys',
+    name: 'IRyS',
+    affiliations: ['Promise']
+  },
+  {
+    id: 'ouro_kronii',
+    name: 'オーロ・クロニー',
+    affiliations: ['Promise']
+  },
+  {
+    id: 'hakos_baelz',
+    name: 'ハコス・ベールズ',
+    affiliations: ['Promise']
+  },
+
+  // ===== Advent =====
+  {
+    id: 'shiori_novella',
+    name: 'シオリ・ノヴェラ',
+    affiliations: ['Advent']
+  },
+  {
+    id: 'koseki_bijou',
+    name: '古石ビジュー',
+    affiliations: ['Advent']
+  },
+  {
+    id: 'nerissa_ravencroft',
+    name: 'ネリッサ・レイヴンクロフト',
+    affiliations: ['Advent']
+  },
+  {
+    id: 'fuwawa_abyssgard',
+    name: 'フワワ・アビスガード',
+    affiliations: ['Advent']
+  },
+  {
+    id: 'mococo_abyssgard',
+    name: 'モココ・アビスガード',
+    affiliations: ['Advent']
+  },
+
+  // ===== ReGLOSS =====
   {
     id: 'otonose_kanade',
     name: '音乃瀬奏',
-    generation: ''
+    affiliations: ['ReGLOSS']
+  },
+  {
+    id: 'ichijou_ririka',
+    name: '一条莉々華',
+    affiliations: ['ReGLOSS']
+  },
+  {
+    id: 'juufuutei_raden',
+    name: '儒烏風亭らでん',
+    affiliations: ['ReGLOSS']
+  },
+  {
+    id: 'todoroki_hajime',
+    name: '轟はじめ',
+    affiliations: ['ReGLOSS']
   }
 ];
